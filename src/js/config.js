cba07ec45a04ece70d4ac94dd06ad2c5981ad58a
Walber Melo <dev4wm@gmail.com>
2023-01-16 19:30:50 +0000

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '7c466893-f03f-4d1c-8203-a6e7ae7d01ea';
+export const MODAL_CLOSE_SEC = 2.5;
